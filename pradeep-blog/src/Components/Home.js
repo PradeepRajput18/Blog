9b6ba38cfcd8b980f63447851e1b133d4b92d7e7
Pradeep Rekapalli <rekkapallipradeepg@gmail.com>
2025-06-09 12:48:51 -0400

    added components Navbar and home

---
+++ pradeep-blog/src/Components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Welcome to the Pradeep Rekapalli Blog</h2>
+            <p>This is a simple blog application built with React.</p>
+            <p>Feel free to explore the blog posts and learn more about various topics.</p>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ pradeep-blog/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Pradeep Rekapalli Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
